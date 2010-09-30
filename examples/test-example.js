86a6f6150ce4976d7c1a407b1ed436803cfa8d4a
Kadir Pekel <kadirpekel@gmail.com>
2010-09-30 23:36:33 +0300

    progress on implementation by adding async testing support

--- examples/test-example.js
+++ examples/test-example.js
@@ -1,14 +1,42 @@
-module.exports = {
-  setup: function () {
-    this.foo = 'bar';
-  },
-  testA: function (assert) {
-    assert.equal(this.foo, 'bar', 'This must pass');    
-  },
-  testB: function (assert) {
-    assert.equal(this.foo, 'baz', 'This must fail');
-  },
-  teardown: function () {
-    delete this.foo;
-  }
-}
+var assert = require('assert'),
+  fs = require('fs');
+
+exports.setup = function (done) {
+  this.foo = 'bar';
+  done();
+};
+
+exports.testSync1 = function (done) {
+  assert.strictEqual('123', 123, "This must fail with this assertion message");
+  done();
+};
+
+exports.testAsync1 = function (done) {
+  var self = this;
+  fs.readFile('./foo', function (err, data) {
+    if (err) assert.fail(err);
+    assert.ok(data);
+    assert.equal(self.foo, data.toString());
+    done();
+  });
+};
+
+exports.testSync2 = function (done) {
+  assert.equal('123', 123);
+  done();
+};
+
+exports.testAsync2 = function (done) {
+  fs.readFile('./absent', function (err, data) {
+    if (err) assert.ifError(err);
+    assert.ok(data);
+    assert.equal(self.foo, data.toString());
+    done();
+  });
+};
+
+exports.teardown = function (done) {
+  // throw new Error('Something went wrong');
+  delete this.foo;
+  done();
+};
